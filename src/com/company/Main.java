eaeb6dc9b0a0299589776d79161ca744f2ae33cb
knud21kea <grah0070@stud.kea.dk>
2021-09-12 18:46:39 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("This is a new line");
+        System.out.println("Blue " + "Red " + "Green ");
+        System.out.println("""
+                Apple
+                    Banana
+                        Orange
+                Chocolate
+                """);
+
+    }
+}
